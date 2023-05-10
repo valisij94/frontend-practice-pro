67dc61b316a02f2fb9375a6a000a9ebafee35e5c
Davy <davyd.dabahian@cheetahdigital.com>
2023-05-10 23:12:39 +0300

    Материалы по 8-му занятию

---
+++ practice_08/js/main.js
@@ -0,0 +1,53 @@
+// Подопытные данные
+
+// Массив чисел (примитивы)
+const numbers = [1,3,6,2,4,0,9,7,5,10,15,12,11];
+
+// Массив строк в одинаковом регистре
+const sameCaseStringsArray = ["these", "strings", "have", "the", "same", "case"];
+
+// Массив строк в разных регистрах
+const differentCaseStringsArray = ["This", "is", "an", "Array", "of", "strings"];
+
+// Объекты для массива объектов
+const ball = {
+    name: "Ball",
+    category: "Sport",
+    img: "./assets/ball.jpeg",
+    price: 100
+};
+const gloves = {
+    name: "Gloves",
+    category: "Sport",
+    img: "./assets/gloves.jpg",
+    price: 140
+};
+const shoes = {
+    name: "Sport shoes",
+    category: "Sport",
+    discount: true,
+    img: "./assets/shoes.png",
+    price: 320
+};
+const hammer = {
+    name: "Hammer",
+    category: "Tools",
+    img: "./assets/hammer.jpeg",
+    price: 40
+};
+const saw = {
+    name: "Saw",
+    category: "Tools",
+    discount: true,
+    img: "./assets/saw.jpeg",
+    price: 75
+};
+const shark = {
+    name: "Shark toy",
+    category: "HomeStuff",
+    img: "./assets/shark.jpeg",
+    price: 45
+};
+
+// Массив объектов
+let products = [ball, gloves, shoes, hammer, saw, shark];
