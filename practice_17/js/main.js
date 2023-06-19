9f5661cce258f9dc08e7c14620ba62a21ce3e159
Davy <davyd.dabahian@cheetahdigital.com>
2023-06-19 23:14:08 +0300

    материалы по занятию 17 - наследование в JavaScript

---
+++ practice_17/js/main.js
@@ -0,0 +1,12 @@
+const productsContainerEl = document.querySelector('#productsContainer');
+
+// const productsArray = [
+//     new PowerTool(1, 'Electric Saw', 200, 'Druzhba forever!', 2.2, 2),
+//     new PowerTool(2, 'Drill', 150, 'Bosch drill', 1.4, 3),
+//     new Tool(3, 'Hammer', 80, 'Super hammer', 55 ),
+// ];
+
+// productsArray.forEach(product => {
+//     const productEl = product.render();
+//     productsContainerEl.append(productEl);
+// })
