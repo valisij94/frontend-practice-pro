7ddeaa33b918b018e88a1af82011d72ed073d227
Davy <davyd.dabahian@cheetahdigital.com>
2023-06-27 00:20:54 +0300

    материалы 19-го занятия: this, дескрипторы свойств

---
+++ practice_19/js/main.js
@@ -0,0 +1,59 @@
+class Product {
+
+    constructor(id, name, price, description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
+
+class PowerTool extends Product {
+    constructor(id, name, price, description, power, warranty) {
+        super(id, name, price, description);
+        this.power = power;
+        this.warranty = warranty;
+    }
+}
+
+class Tool extends Product {
+    constructor(id, name, price, description, strength) {
+        super(id, name, price, description);
+        this.strength = strength;
+    }
+}
+
+
+function customMap(callback) {
+    let result = [];
+    for (let i = 0; i < this.length; i++) {
+        result[i] = callback(this[i], i);
+    }
+    return result;
+}
+
+const weirdArray = {
+    length: 2,
+    0: 'Saw',
+    1: 'Hammer'
+}
+
+const veryWeirdArray = {
+    0: 'Drill',
+    1: 'Axe',
+    2: 'Nails',
+};
+
+function validateInput(errorText) {
+    if (this.required) {
+        if (!this.value) {
+            this.classList.add('error');
+            this.value = errorText;
+        }
+    }
+}
+
+const inputs = document.querySelectorAll('input');
+inputs.forEach( input => {
+    validateInput.call(input, "Fill this immediately!");
+});
