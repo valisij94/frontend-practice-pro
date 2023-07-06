b1eabccfc091b9837a3a2482b656c2c47bd3ae8b
Davy <davyd.dabahian@cheetahdigital.com>
2023-07-06 15:31:18 +0300

    результаты 22го занятия

--- practice_21/js/main.js
+++ practice_21/js/main.js
@@ -31,3 +31,95 @@ function renderProducts(products) {
         productsContainerEl.append(productEl);
     });
 };
+
+/*
+1. Ставим обработчик на событие загрузки документа (`DOMContentLoaded`). Нужно вывести в консоль текущее время (форматированное по умолчанию). Чтобы получить строку с текущим временем, используем `new Date()`.
+*/
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log(new Date());
+// });
+
+/*
+2. Пишем функцию `requestProducts`. Она должна запрашивать с бэка (`https://fakestoreapi.com/products`) список товаров, преобразовать результат в JSON, и передать результат в функцию `renderProducts`. То есть, наша задача - получить данные товаров, и отрендерить их.
+*/
+
+// function requestProducts() {
+//     fetch('https://fakestoreapi.com/products')
+//       .then(response => response.json())
+//       .then(data => renderProducts(data))
+//       .catch(err => console.log('Something went wrong!'));
+// }
+
+async function requestProducts() {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const jsonData = await response.json();
+        renderProducts(jsonData);
+    }
+    catch(err) {
+        console.log('Error!')
+    }
+}
+
+/*
+3. Добавляем вызов `requestProducts` в событие загрузки документа (п.1). То есть, нам нужно загружать наши товары при загрузке документа и готовности DOM.
+*/
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     requestProducts();
+// });
+
+/*
+4. Работаем с текстовым полем `input#testInput`. Наша задача - протоколировать нажатие каждой кнопки (выводить в консоль ее код).
+ - нужен обработчик события нажатия кнопки на инпут. Событие - `keypress`
+ - в объекте события, есть свойство `charCode` - это код нажатой кнопки. Его нужно вывести в консоль.
+ */
+
+//  const testInput = document.querySelector('#testInput');
+//  testInput.addEventListener('keypress', (event) => {
+//     console.log(event.charCode);
+//  });
+
+/*
+5. Пишем свою функцию валидации для инпута, назовем ее `validateInput`. Предположим, мы вводим количество единиц товара, и у нас разрешенные символы - только цифры. Наша задача - запретить ввод в текстовое поле не-цифровых символов.
+ - нужен обработчик на `keypress` текстового поля - это будет наша функция `validateInput`
+ - при обрабокте события, проверяем `event.charCode`.
+ - коды клавиш-цифр - от 48 до 57.
+ - здесь нам нужно ПРЕДОТВРАТИТЬ стандартное поведение (т.е. изменение текста инпута). В этом нам поможет `preventDefault`.
+ */
+
+ function validateInput(event) {
+    if (event.charCode > 57 || event.charCode < 48) {
+        event.preventDefault();
+    }
+ }
+
+ const testInput = document.querySelector('#testInput');
+//  testInput.addEventListener('keypress', validateInput);
+
+//  setTimeout( () => {
+//     testInput.removeEventListener('keypress', validateInput);
+//     console.log('removed')
+//  }, 10000);
+
+/*
+6. Усложняем валидацию. Теперь мы хотим проверять правильность ввода при "уходе" фокуса из текстового поля. Что требуется:
+ - закомментировать добавление обработчика `keypress` на инпут
+ - добавить обработчик события утери фокуса (`focusout`) на наш инпут
+ - при обработке события, проверять текст инпута (через `event.target.value`)
+ - если текст не является числом (используем метод `isNaN`), то возвращаем фокус в наше текстовое поле (для этого, у DOM-элементов есть метод `focus` - то есть нам нужно вызвать этот метод у инпута).
+ */
+
+testInput.addEventListener('focusout', (event) => {
+    if (isNaN(event.target.value)) {
+        event.target.focus();
+        event.target.classList.add('error-input');
+    }
+    else {
+        event.target.classList.remove('error-input');
+    }
+});
+
+/*
+7. Добавляем визуализации - расширяем обработчик события утери фокуса у инпута. Теперь, если в инпуте не число - то применяем к инпуту класс `error-input`. Если число валидное - то этот класс убираем.
+*/
--- practice_22/js/main.js
+++ practice_22/js/main.js
@@ -2,3 +2,81 @@ let storageObject = {
     click: 0,
     keypress: 0
 }
+
+/*
+1. Пишем функцию валидации текстового поля `validateInput`. Она должна принимать объект Event, и проверять, что поле не пустое (т.е там введено какое-либо значение). Если поле пустое - применяем к полю класс error-input. Если непустое - убираем этот класс.
+*/
+
+function validateInput(event) {
+    if (event.target.value) {
+        event.target.classList.remove('error-input');
+    }
+    else {
+        event.target.classList.add('error-input');
+    }
+}
+
+/*
+2. Добавляем обработчик на событие `focusout` у инпута `input#titleInput`.
+*/
+// const titleInput = document.querySelector('#titleInput');
+// const titleInput = document.getElementById('titleInput');
+// titleInput.addEventListener('focusout', validateInput);
+
+/*
+3. Усложняем задачу. Нам нужно на все инпуты, которые обязательны к заполнению, добавить валидацию на непустоту. У всех инпутов, обязательных к заполнению, есть атрибут `data-required`. То есть, нам нужно:
+ - пройтись по всем инпутам в документе // querySelectorAll
+ - у каждого инпута проверить наличие атрибута `data-required`.
+ elem.required // FOR STANDARD ATTRIBUTES!
+ elem.dataset.required // ONLY FOR DATA-ATTRIBUTES!!!
+ - если такой атрибут есть - то на событие `focusout` добавляем обработчик - `validateInput`.
+ - если такого атрибута нет - ничего не делаем.
+ */
+
+ const inputs = document.querySelectorAll('input');
+ inputs.forEach( input => {
+    if (input.dataset.required) {
+        input.addEventListener('focusout', validateInput);
+    }
+ });
+
+/*
+4. Пишем функцию `checkNumber` - она должна принять объект event, и проверить, является ли `event.target.value` числом. Используем `isNaN`. Если число - то возвращаем `true`, иначе - `false`.
+*/
+
+// function checkNumber(event) {
+//     if (isNaN(event.target.value)) {
+//         event.target.classList.add('error-input');
+//     }
+//     else {
+//         event.target.classList.remove('error-input');
+//     }
+// }
+
+/*
+5. Задача со звездочкой (обсуждаем и потом решаем). Нам нужно добавить валидацию введенного числа в инпуты c атрибутом data-isnumber. Валидировать также нужно на событие `focusout`. Это можно сделать или отдельным обработчиком, или доработать решение задачи 3.
+*/
+
+//  inputs.forEach( input => {
+//     if (input.dataset.required) {
+//         input.addEventListener('focusout', validateInput);
+//     }
+//     if (input.dataset.isnumber) {
+//         input.addEventListener('focusout', checkNumber);
+//     }
+//  });
+
+/*
+6. Вспоминаем `preventDefault`. У нас под инпутами, есть кнопка "Отправить". Кнопка относится к форме с инпутами, и должна отправлять форму на сервер.
+ - добавить обработчик на событие клика на эту "кнопку".
+ - при клике на кнопку, нужно проверять, есть ли у нас невалидные инпуты (например, поискать в форме элементы с классом `error-input`). если у нас есть невалидные инпуты - предотвращаем поведение по умолчанию. // preventDefault()
+ */
+// const sendBtn = document.querySelector('button.button');
+// const form = document.querySelector('form#testForm');
+
+// sendBtn.addEventListener('click', (event) => {
+//     const inputs = form.querySelectorAll('input.error-input');
+//     if (inputs.length > 0) {
+//         event.preventDefault();
+//     }
+// });
